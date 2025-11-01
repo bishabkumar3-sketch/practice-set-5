ab356d8086b24b72dc187acadb6313a874db020e
bishab kumar <bishabkumar3@gmail.com>
2025-11-01 17:05:50 +0530

    Add files via upload

---
+++ 1 ouestion.py
@@ -0,0 +1,13 @@
+# Write a program to create a dictionary of  hindi words with values as their english translation provide user with an option to look it 
+
+words = {
+    "madad": "help",
+    "kursi": "chair",
+    "billi": "cat",
+    "papa": "dad",
+    "mummuy": "mom"
+}
+
+word = input("Enter the word you want meaning of: ")
+
+print(words[word])
